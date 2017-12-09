34e229161d9554b21a4727418764f8fb5cb45bfa
Freyj <aerandil@gmail.com>
2017-12-09 22:33:27 +0100

    Client un peu mieux

--- ASA-Cadabra/src/fr/alma2017/client/Client.java
+++ ASA-Cadabra/src/fr/alma2017/client/Client.java
@@ -1,49 +1,74 @@
 package fr.alma2017.client;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import fr.alma2017.api.client.IClient;
 import fr.alma2017.api.composant.IComposant;
-import fr.alma2017.api.composant.IInterfaceComposantFournie;
-import fr.alma2017.api.composant.IInterfaceComposantRequise;
+import fr.alma2017.composantClass.AComposant;
 
-public class Client implements IComposant, IClient {
+public class Client extends AComposant implements IComposant, IClient {
 	
 	private String message;
+	private String motDePasse;
+	private String utilisateur;
 
-	
+	/**
+	 * Constructeur vide
+	 */
 	public Client() {
-		message = "No message";
+		message = "";
+		utilisateur = "";
+		motDePasse = "";
 	}
-
-	@Override
-	public IInterfaceComposantFournie getInterfaceFournie() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	/**
+	 * Constructeur pour un utilisateur complet
+	 * @param uti un String avec le nom de l'utilisateur
+	 * @param mdp un String avec le mot de passe
+	 * @param mess un String avec le message à envoyer
+	 */
+	public Client(String uti, String mdp, String mess) {
+		message = mess;
+		utilisateur = uti;
+		motDePasse = mdp;
 	}
-
-	@Override
-	public IInterfaceComposantRequise getInterfaceRequise() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<IComposant> getComposantsInternes() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
+	//Getters & Setters
+	public String getMessage() {
+		return message;
+	}	
 	
 	public void setMessage(String mes) {
 		message = mes;
 	}
 	
-	public String getMessage() {
-		return message;
+	public void setMotDePasse(String mdp) {
+		motDePasse = mdp;
 	}
 	
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+	
+	public void setNomUtilisateur(String ut) {
+		utilisateur = ut;
+	}
+	
+	public String getNomUtilisateur() {
+		return utilisateur;
+	}
+
+	/**
+	 * Méthode qui va servir à envoyer un message via les interfaces pour remonter l'archi
+	 */
 	public void sendMessage() {
-		//this.getInterfaceRequise().sendRequest(message);
+		//format de l'envoi : utilisateur + " " + motdepasse + " " + message
+		ArrayList<String> infosEnvoyees = new ArrayList<String>();
+		infosEnvoyees.add(getNomUtilisateur());
+		infosEnvoyees.add(getMotDePasse());
+		infosEnvoyees.add(getMessage());
+		//this.getInterfaceRequise().sendRequest(infosEnvoyees);
+		
 	}
 
 
