f1d33e4d20620bcebf68f0133761b736f137ba90
Charlene <charlene.servantie@etu.univ-nantes.fr>
2017-12-09 16:12:24 +0100

    Begin client and clientserverconfig
    and refactor method

--- ASA-Cadabra/src/fr/alma2017/client/Client.java
+++ ASA-Cadabra/src/fr/alma2017/client/Client.java
@@ -1,7 +1,37 @@
 package fr.alma2017.client;
 
+import java.util.List;
+
 import fr.alma2017.api.composant.IComposant;
+import fr.alma2017.api.composant.IInterfaceComposantFournie;
+import fr.alma2017.api.composant.IInterfaceComposantRequise;
 
 public class Client implements IComposant {
+
+	@Override
+	public IInterfaceComposantFournie getInterfaceFournie() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IInterfaceComposantRequise getInterfaceRequise() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<IComposant> getInnerComposants() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/**
+	 * Le clien est un main à part entière
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
 
 }
