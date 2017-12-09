58749479795f99f21dec813a6be486cee1fb2706
Freyj <aerandil@gmail.com>
2017-12-09 22:17:36 +0100

    Ajout de la config client

---
+++ ASA-Cadabra/src/fr/alma2017/client/ClientConfiguration.java
@@ -0,0 +1,36 @@
+package fr.alma2017.client;
+
+import java.util.List;
+
+import fr.alma2017.api.composant.IComposant;
+import fr.alma2017.api.configuration.IConfiguration;
+import fr.alma2017.api.configuration.IInterfaceConfiguration;
+import fr.alma2017.api.connecteur.IConnecteur;
+
+public class ClientConfiguration implements IConfiguration{
+
+	@Override
+	public void notify(Object source) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public IInterfaceConfiguration getInterface() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<IConnecteur> getConnecteurs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<IComposant> getComposantsInternes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
