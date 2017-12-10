be45b715664e6929323f5ba51e0f8606ae503dcf
Freyj <aerandil@gmail.com>
2017-12-10 13:18:21 +0100

    Remontage interface

--- ASA-Cadabra/src/fr/alma2017/api/server/IBaseDonnees.java
+++ ASA-Cadabra/src/fr/alma2017/api/server/IBaseDonnees.java
@@ -3,5 +3,6 @@ package fr.alma2017.api.server;
 import fr.alma2017.api.composant.IComposant;
 
 public interface IBaseDonnees extends IComposant {
-
+	String getMotDePasseFromUtilisateur(String utilisateur);
+	void addUtilisateur(String utilisateur, String motDePasse);
 }
