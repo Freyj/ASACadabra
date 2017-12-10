21fe13a9c300bc83224092189aad47b3a9bf08dc
Freyj <aerandil@gmail.com>
2017-12-10 23:38:46 +0100

    Knowledge, come to meeeee

--- ASA-Cadabra/src/fr/alma2017/proxy/ProxyConnectionManager.java
+++ ASA-Cadabra/src/fr/alma2017/proxy/ProxyConnectionManager.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import fr.alma2017.api.IObserver;
+import fr.alma2017.clientServer.Main;
 
 public class ProxyConnectionManager implements InvocationHandler{
 
@@ -27,7 +28,17 @@ public class ProxyConnectionManager implements InvocationHandler{
 			//Fonctionne correctement
 			ret = Void.TYPE;
 			this.observer.add( (IObserver) args[0] );
-		}else if(method.getName().substring(0, 3).equals("set") && this.observer != null){
+		}
+		else if (method.getName().equals("requestConnection") && this.observer != null) {
+			ret = method.invoke(this.target, args);
+			if(Main.Sysout) {
+				System.out.println("Proxy ConnectionManager : " + this.target.getClass().getName() + " est observe par " + this.observer.size() + " objets.");
+			}
+			for(IObserver observer : this.observer) {
+				observer.notify(target);
+			}
+		}	
+		else if(method.getName().substring(0, 3).equals("set") && this.observer != null){
 			ret = method.invoke(this.target, args);
 			//this.observer.notify(this.target);
 			System.out.println(target.getClass().getName() + " ["+ method.getName().substring(3) + "=" + args[0] + "] is modified");
