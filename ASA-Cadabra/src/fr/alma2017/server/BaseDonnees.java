38e166110d0c0ff3fd878a587ecfdef50d8424e2
Freyj <aerandil@gmail.com>
2017-12-10 12:35:21 +0100

    BDD
    with amazing security and no file \o/

--- ASA-Cadabra/src/fr/alma2017/server/BaseDonnees.java
+++ ASA-Cadabra/src/fr/alma2017/server/BaseDonnees.java
@@ -2,6 +2,8 @@ package fr.alma2017.server;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+
 import fr.alma2017.api.composant.IComposant;
 import fr.alma2017.api.server.IBaseDonnees;
 import fr.alma2017.composantClass.AComposant;
@@ -9,14 +11,48 @@ import fr.alma2017.composantClass.InterfaceComposantFournie;
 
 public class BaseDonnees extends AComposant implements IComposant, IBaseDonnees {
 
+	private HashMap<String, String> valuesUsers;
+	
 	public BaseDonnees() {
-		super();
 		List<Class<?>> portFournis = new ArrayList<Class<?>>();
 		List<Class<?>> serviceFournis = new ArrayList<Class<?>>();
 		portFournis.add(IBaseDonnees.class);
 		serviceFournis.add(IBaseDonnees.class);
 		this.interfaceFournie = new InterfaceComposantFournie(portFournis, serviceFournis);
+		valuesUsers = new HashMap<String, String>();
+		//ajout d'un user pour l'example
+		//tout en clair, vive la sécurité \o/
+		addUtilisateur("bob", "example");
 	}
+	
+	/**
+	 * Renvoie le mdp de l'utilisateur demandé
+	 * @param utilisateur
+	 * @return Si il n'existe pas, renvoie un String vide
+	 */
+	public String getMotDePasseFromUtilisateur(String utilisateur) {
+		if (valuesUsers.containsKey(utilisateur)) {
+			return valuesUsers.get(utilisateur);
+		}
+		else {
+			return "";
+		}
+	}
+	
+	/**
+	 * Ajout d'un utilisateur dans la "base de données"
+	 */
+	public void addUtilisateur(String nom, String mdp) {
+		if (!valuesUsers.containsKey(nom)) {
+			valuesUsers.put(nom, mdp);
+		}
+		else {
+			//change with an exception some day
+			System.out.println("Utilisateur déjà existant");
+		}
+	}
+	
+	
 	
 	
 }
